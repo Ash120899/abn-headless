1ce6189b4f913974731de213e2faadbf91c73762
Ash120899 <ashuupadhyay0812@gmail.com>
2026-05-23 21:06:59 +0530

    check blog routing

---
+++ src/app/blog/[slug]/page.js
@@ -0,0 +1,8 @@
+export default function BlogPost({ params }) {
+  return (
+    <div style={{ padding: "100px", color: "white", background: "black" }}>
+      <h1>Blog Slug:</h1>
+      <p>{params.slug}</p>
+    </div>
+  );
+}
